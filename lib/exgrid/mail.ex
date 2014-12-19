1f4fc58efd0517b6aee5579c799ca424d8fd1b80
DanX <danx.exe@gmail.com>
2014-12-19 11:37:04 -0200

    Remove struct keys

--- lib/exgrid/mail.ex
+++ lib/exgrid/mail.ex
@@ -7,6 +7,10 @@ defmodule ExGrid.Mail do
     {code, body} = HTTPHandler.post(creds, url, build_form_data(creds, message))
   end
 
+  defp build_form_data(%ExGrid{} = creds, %ExGrid.Message{} = message) do
+    build_form_data(Map.from_struct(creds), Map.from_struct(message))
+  end
+
   defp build_form_data(creds, message) do
     full_message = Map.merge(creds, message)
     Enum.map(Map.to_list(full_message), fn {k,v} -> ("#{k}=#{v}") end ) |>
