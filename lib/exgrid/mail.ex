b22406fc5a95642773f248b04130314672cc8278
DanX <danx.exe@gmail.com>
2014-12-19 11:43:13 -0200

    Support name + email pair on from and to fields

--- lib/exgrid/mail.ex
+++ lib/exgrid/mail.ex
@@ -13,11 +13,31 @@ defmodule ExGrid.Mail do
 
   defp build_form_data(creds, message) do
     full_message = Map.merge(creds, message)
+
+    {name_from, email_from} = split_email(full_message.from)
+    {name_to, email_to} = split_email(full_message.to)
+
+    full_message = Map.merge(full_message, %{
+      fromname: name_from,
+      from: email_from,
+      toname: name_to,
+      to: email_to
+    })
+
     Enum.map(Map.to_list(full_message), fn {k,v} -> ("#{k}=#{v}") end ) |>
     Enum.join("&")
   end
 
   defp url do
     "https://sendgrid.com/api/mail.send.json"
+  end
+
+  defp split_email(nil) do
+    {nil, nil}
+  end
+
+  defp split_email(full_email) do
+    [_, name, email] = Regex.run(~r/^([^<]*?)\s*?<([^>]*)>/, full_email) || [nil, nil, full_email]
+    {name, email}
   end
 end
