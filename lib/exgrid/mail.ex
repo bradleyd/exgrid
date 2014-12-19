0e944f48499d50ffbffd368d39d8eac56516d1e5
DanX <danx.exe@gmail.com>
2014-12-19 14:30:05 -0200

    Support for multiple recipients

--- lib/exgrid/mail.ex
+++ lib/exgrid/mail.ex
@@ -24,12 +24,23 @@ defmodule ExGrid.Mail do
       to: email_to
     })
 
-    Enum.map(Map.to_list(full_message), fn {k,v} -> ("#{k}=#{v}") end ) |>
-    Enum.join("&")
+    Enum.map(Map.to_list(full_message), fn {k,v} ->
+      case v do
+        v when is_list(v) -> v |> Enum.map(fn vv -> "#{k}[]=#{vv}" end) |> Enum.join("&")
+        _ -> "#{k}=#{v}"
+      end
+    end)
+    |> Enum.join("&")
   end
 
   defp url do
     "https://sendgrid.com/api/mail.send.json"
+  end
+
+  defp split_email(emails) when is_list(emails) do
+    emails = (Enum.map emails, fn(email) -> split_email email end)
+    |> (Enum.reduce {[], []}, fn(pair, acc) -> {[elem(pair, 0) | elem(acc, 0)], [elem(pair, 1) | elem(acc, 1)]} end)
+    |> (fn pair -> {elem(pair, 0) |> Enum.reverse, elem(pair, 1) |> Enum.reverse} end).()
   end
 
   defp split_email(nil) do
