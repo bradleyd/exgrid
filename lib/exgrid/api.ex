0f8206ea75d437d663d07b437ed4a335368e4082
DanX <danx.exe@gmail.com>
2015-01-05 11:21:14 -0200

    Move encoding logic to API module

--- lib/exgrid/api.ex
+++ lib/exgrid/api.ex
@@ -3,22 +3,47 @@ defmodule ExGrid.API do
   Build API endpoint URL
   """
   def build_url(context, verb) do
-    "https://api.sendgrid.com/api/" <> context <> "." <> verb <> ".json?"
+    "https://api.sendgrid.com/api/" <> context <> "." <> verb <> ".json"
   end
-  def build_url(context, verb, query_params) do
-    "https://api.sendgrid.com/api/" <> context <> "." <> verb <> ".json?" <> build_form_data(query_params)
+  def build_url(context, verb, credentials) do
+    build_url(context, verb) <> "?" <> build_form_data(credentials)
+  end
+  def build_url(context, verb, credentials, params) do
+    build_url(context, verb) <> "?" <> build_form_data(credentials, params)
   end
 
   @doc """
   Build Form data for GET or POST
   """
-  def build_form_data(creds) do
-    Enum.map(Map.to_list(creds), fn {k,v} -> ("#{k}=#{v}") end ) |>
-    Enum.join("&")
+  def build_form_data(%ExGrid{} = creds, %{__struct__: _} = params) do
+    build_form_data(Map.from_struct(creds), Map.from_struct(params))
   end
-  def build_form_data(creds, message) do
-    full_message = Map.merge(creds, message)
-    Enum.map(Map.to_list(full_message), fn {k,v} -> ("#{k}=#{v}") end ) |>
-    Enum.join("&")
+  def build_form_data(%ExGrid{} = creds, params) do
+    build_form_data(Map.from_struct(creds), params)
   end
+  def build_form_data(%ExGrid{} = creds) do
+    build_form_data(Map.from_struct(creds))
+  end
+  def build_form_data(creds, params) do
+    build_form_data Map.merge(creds, params)
+  end
+  def build_form_data(args) do
+    Enum.map(Map.to_list(args), fn {k,v} -> encode_attribute(k, v) end)
+    |> Enum.join("&")
+  end
+
+  @doc """
+  URL encode key / value pair
+  """
+  def encode_attribute(k, v) when is_list(v) do
+    v
+    |> Enum.map(fn vv -> "#{k}[]=#{encode_value(vv)}" end)
+    |> Enum.join("&")
+  end
+  def encode_attribute(k, v), do: "#{k}=#{encode_value(v)}"
+
+  @doc """
+  URL encode a value
+  """
+  def encode_value(v), do: URI.encode_www_form("#{v}")
 end
--- lib/exgrid/mail.ex
+++ lib/exgrid/mail.ex
@@ -1,44 +1,23 @@
 defmodule ExGrid.Mail do
-  
+  import ExGrid.API
   alias ExGrid.HTTPHandler
 
-
   def send(creds, message) do
-    {_code, _body} = HTTPHandler.post(creds, url, build_form_data(creds, message))
+    {_code, _body} = HTTPHandler.post(creds, build_url("mail", "send"), build_mail_form_data(creds, message))
   end
 
-  defp build_form_data(%ExGrid{} = creds, %ExGrid.Message{} = message) do
-    build_form_data(Map.from_struct(creds), Map.from_struct(message))
-  end
+  defp build_mail_form_data(creds, message) do
+    {name_from, email_from} = split_email(message.from)
+    {name_to, email_to} = split_email(message.to)
 
-  defp build_form_data(creds, message) do
-    full_message = Map.merge(creds, message)
-
-    {name_from, email_from} = split_email(full_message.from)
-    {name_to, email_to} = split_email(full_message.to)
-
-    full_message = Map.merge(full_message, %{
+    message = Map.merge(message, %{
       fromname: name_from,
       from: email_from,
       toname: name_to,
       to: email_to
     })
 
-    Enum.map(Map.to_list(full_message), fn {k,v} -> encode_attribute(k, v) end)
-    |> Enum.join("&")
-  end
-
-  def encode_value(v), do: URI.encode_www_form("#{v}")
-
-  def encode_attribute(k, v) when is_list(v) do
-    v
-    |> Enum.map(fn vv -> "#{k}[]=#{encode_value(vv)}" end)
-    |> Enum.join("&")
-  end
-  def encode_attribute(k, v), do: "#{k}=#{encode_value(v)}"
-
-  defp url do
-    "https://sendgrid.com/api/mail.send.json"
+    build_form_data(creds, message)
   end
 
   defp split_email(emails) when is_list(emails) do
@@ -46,11 +25,9 @@ defmodule ExGrid.Mail do
     |> (Enum.reduce {[], []}, fn(pair, acc) -> {[elem(pair, 0) | elem(acc, 0)], [elem(pair, 1) | elem(acc, 1)]} end)
     |> (fn pair -> {elem(pair, 0) |> Enum.reverse, elem(pair, 1) |> Enum.reverse} end).()
   end
-
   defp split_email(nil) do
     {nil, nil}
   end
-
   defp split_email(full_email) do
     [_, name, email] = Regex.run(~r/^([^<]*?)\s*?<([^>]*)>/, full_email) || [nil, nil, full_email]
     {cleanup_name(name), email}
