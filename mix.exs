27afbd56df4e7d94413b0ce1fe55fe6ef7461c91
Sean Stasica <sean@parkifi.com>
2016-08-24 11:03:15 -0600

    remove tag from ibrowse

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule ExGrid.Mixfile do
   end
 
   defp deps do
-    [ {:ibrowse, tag: "~> 4.2"},
+    [ {:ibrowse, "~> 4.2"},
       {:httpotion, "~> 1.0.0"},
       {:json, "~> 0.3.2"},
       {:timex, "~> 2.2"}
